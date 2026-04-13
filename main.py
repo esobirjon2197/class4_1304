84ac08a79c36b24b633b68af1550a670bfd6d235
Sobirjon Eshniyazov <esobirjon2197@gmail.com>
2026-04-13 17:29:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+#  1-m
+class Mashina:
+    gildirak = 4
+
+m1 = Mashina()
+print(f"m1 gildirak: {m1.gildirak}")
+
+m2 = Mashina()
+m2.rang = "Yashil"
+print(f"m2 rang: {m2.gildirak}")
+
+
+m3 = Mashina()
+m3.madel = "Damas"
+print(f"m3 madel: {m3.madel}")
+
+
+# 2-m
+class Telefon:
+    brand = "Samsung"
+
+
+t1 = Telefon()
+print(f"t1.brand: {t1.brand}")
+
+t2 = Telefon()
+t2.narx = 1234
+print(f"t2.narx: {t2.narx}")
+
+t3 = Telefon()
+t3.xotira = 128
+print(f"t3.xotira: {t3.xotira}")
+
+
+# 3-m
+class Kitob:
+    sahifa = 200
+
+k1 = Kitob()
+print(f"Kitob sahifa: {k1.sahifa}")
+
+k2 = Kitob()
+k2.nomi = "Harry"
+print(f"Kitob nomi: {k2.nomi}")
+
+
+k3 = Kitob()
+k3.muallif = "Mualif yo'q"
+print(f"Kitob mualif: {k3.muallif}")
+
+
+
+# 4-m
+class Talaba:
+    kurs = 1
+
+t1 = Talaba()
+print(f"kurs: {t1.kurs}")
+
+t2= Talaba()
+t2.ism = "Bunyod"
+print(f"ism: {t2.ism}")
+
+t3 = Talaba()
+t3.yosh = 15
+print(f"yosh: {t3.yosh}")
+
+
+
+# 5-m
+class Kompyuter:
+    ram = 8
+
+k1 = Kompyuter()
+print(f"ram : {k1.ram}")
+
+k2 = Kompyuter()
+k2.protsesor = 18
+print(f"protsesor : {k2.protsesor}")
+
+k3 = Kompyuter()
+k3.ssd = 2
+print(f"ssd : {k3.ssd}")
+
